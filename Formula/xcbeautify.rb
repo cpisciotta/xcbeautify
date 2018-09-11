da6e9aed2ad92438a7f5c7f0d2fabd0ede62b885
Thi <t@thi.im>
2018-09-12 00:45:57 +0900

    Fix brew detects wrong version from install URL

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -1,7 +1,7 @@
 class Xcbeautify < Formula
-  version = "0.1.0"
   desc "A little beautifier tool for xcodebuild"
   homepage "https://github.com/thii/xcbeautify"
+  version "0.1.0"
   url "https://github.com/thii/xcbeautify/releases/download/#{version}/xcbeautify-#{version}-x86_64-apple-macosx10.10.zip"
   sha256 "e1f390dc565b36a3a7fa4836064fbc21ab03bfebba2cd0ac4e98c927ec4bda2d"
   head "https://github.com/thii/xcbeautify.git"
