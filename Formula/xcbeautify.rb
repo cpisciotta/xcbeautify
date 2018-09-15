184e6ddd8dbbb796bc51e5820956ed580d194010
Thi <t@thi.im>
2018-09-15 23:10:39 +0900

    Release version 0.2.0

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -1,9 +1,9 @@
 class Xcbeautify < Formula
-  desc "A little beautifier tool for xcodebuild"
+  version = "0.2.0"
   homepage "https://github.com/thii/xcbeautify"
   version "0.1.0"
   url "https://github.com/thii/xcbeautify/releases/download/#{version}/xcbeautify-#{version}-x86_64-apple-macosx10.10.zip"
-  sha256 "e1f390dc565b36a3a7fa4836064fbc21ab03bfebba2cd0ac4e98c927ec4bda2d"
+  sha256 "53f6395f86aab6814a461fe1e9bfc7b23bf8cf44fdafd45a9f3503bdaece914e"
   head "https://github.com/thii/xcbeautify.git"
 
   def install
--- Sources/xcbeautify/Version.swift
+++ Sources/xcbeautify/Version.swift
@@ -1,1 +1,1 @@
-let version = "0.1.0"
+let version = "0.2.0"
