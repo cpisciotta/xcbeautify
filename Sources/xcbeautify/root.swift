484691ea5b0ee213319e3727d33b3dd68ccf2ed3
Darren Lai <kinwah.lai@gmail.com>
2020-08-14 00:34:02 +0100

    new outputtype.test and print test specific task/result too under quiet/quieter flag if isCI flag set

--- Sources/xcbeautify/root.swift
+++ Sources/xcbeautify/root.swift
@@ -14,68 +14,56 @@ private func configuration(command: Command) {
               value: false,
               description: "Only print tasks that have warnings or errors.",
               inheritable: true)
-        ])
+    ])
     
     command.add(flags: [
         .init(longName: "quieter",
               value: false,
               description: "Only print tasks that have errors.",
               inheritable: true)
-        ])
-
+    ])
+    
+    command.add(flags: [
+        .init(longName: "isCI",
+              value: false,
+              description: "Print test result too under quiet/quiter flag")
+    ])
+    
     command.add(flags: [
         .init(shortName: "v",
               longName: "version",
               value: false,
               description: "Prints the version",
               inheritable: true)
-        ])
-
+    ])
+    
     command.inheritablePreRun = { flags, args in
         if let versionFlag = flags.getBool(name: "version"), versionFlag == true {
-            print(version)
+            print("Version: \(version)")
             return false
         }
-
+        
         return true
     }
-
+    
     command.example = """
-      $ xcodebuild test -project MyApp.xcodeproj -scheme MyApp -destination 'platform=iOS Simulator,OS=12.0,name=iPhone X' | xcbeautify
+    $ xcodebuild test -project MyApp.xcodeproj -scheme MyApp -destination 'platform=iOS Simulator,OS=12.0,name=iPhone X' | xcbeautify
     """
 }
 
 private func execute(flags: Flags, args: [String]) {
+    
     let parser = Parser()
     let quiet = flags.getBool(name: "quiet") == true
     let quieter = flags.getBool(name: "quieter") == true
-    var lastFormatted: String? = nil
-
+    let isCI = flags.getBool(name: "isCI") == true
+    let output = OutputHandler(quiet: quiet, quieter: quieter, isCI: isCI, { print($0) })
+    
     while let line = readLine() {
         guard let formatted = parser.parse(line: line) else { continue }
-
-        if !quiet && !quieter {
-            print(formatted)
-            continue
-        }
-
-        switch parser.outputType {
-            case OutputType.warning:
-                if quieter {continue}
-            fallthrough
-            case OutputType.error:
-                if let last = lastFormatted {
-                    print(last)
-                    lastFormatted = nil
-                }
-                print(formatted)
-            case OutputType.result:
-                print(formatted)
-            default:
-                lastFormatted = formatted
-        }
+        output.write(parser.outputType, formatted)
     }
-
+    
     if let summary = parser.summary {
         print(summary.format())
     }
