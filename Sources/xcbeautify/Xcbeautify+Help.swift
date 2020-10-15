91f72c5cdaf4c9a4d01df2c98f3ec15c1ecb7be1
Benedek Kozma <cyberbeni@gmail.com>
2020-10-15 18:09:23 +0200

    4 space indentation

--- Sources/xcbeautify/Xcbeautify+Help.swift
+++ Sources/xcbeautify/Xcbeautify+Help.swift
@@ -1,13 +1,13 @@
 import ArgumentParser
 
 extension Xcbeautify {
-	private static var abstract: String = "Format the output of `xcodebuild` and `swift`"
-	private static var additionalHelp: String = """
+    private static var abstract: String = "Format the output of `xcodebuild` and `swift`"
+    private static var additionalHelp: String = """
 	EXAMPLE: xcodebuild test -project MyApp.xcodeproj -scheme MyApp -destination 'platform=iOS Simulator,OS=12.0,name=iPhone X' | xcbeautify
 	"""
-	static var configuration: CommandConfiguration = {
-		return CommandConfiguration(abstract: abstract,
-											 discussion: additionalHelp,
-											 version: version)
-	}()
+    static var configuration: CommandConfiguration = {
+        return CommandConfiguration(abstract: abstract,
+                                    discussion: additionalHelp,
+                                    version: version)
+    }()
 }
--- Sources/xcbeautify/Xcbeautify.swift
+++ Sources/xcbeautify/Xcbeautify.swift
@@ -4,26 +4,26 @@ import XcbeautifyLib
 
 
 struct Xcbeautify: ParsableCommand {
-	@Flag(name: [.short, .long], help: "Only print tasks that have warnings or errors.")
-	var quiet = false
-	
-	@Flag(name: [.customLong("qq", withSingleDash: true), .long], help: "Only print tasks that have errors.")
-	var quieter = false
-	
-	@Flag(name: .long, help: "Print test result too under quiet/quieter flag.")
-	var isCi = false
-	
-	func run() throws {
-		let parser = Parser()
-		let output = OutputHandler(quiet: quiet, quieter: quieter, isCI: isCi, { print($0) })
-		
-		while let line = readLine() {
-			guard let formatted = parser.parse(line: line) else { continue }
-			output.write(parser.outputType, formatted)
-		}
-		
-		if let summary = parser.summary {
-			print(summary.format())
-		}
-	}
+    @Flag(name: [.short, .long], help: "Only print tasks that have warnings or errors.")
+    var quiet = false
+    
+    @Flag(name: [.customLong("qq", withSingleDash: true), .long], help: "Only print tasks that have errors.")
+    var quieter = false
+    
+    @Flag(name: .long, help: "Print test result too under quiet/quieter flag.")
+    var isCi = false
+    
+    func run() throws {
+        let parser = Parser()
+        let output = OutputHandler(quiet: quiet, quieter: quieter, isCI: isCi, { print($0) })
+        
+        while let line = readLine() {
+            guard let formatted = parser.parse(line: line) else { continue }
+            output.write(parser.outputType, formatted)
+        }
+        
+        if let summary = parser.summary {
+            print(summary.format())
+        }
+    }
 }
