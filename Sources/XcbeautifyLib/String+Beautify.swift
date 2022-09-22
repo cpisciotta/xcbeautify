e085ee01c0231106326346996b58eeff82518086
Benedek Kozma <benedek.kozma@tresorit.com>
2022-09-22 16:07:53 +0200

    Parse name and unitname for measurements (#73)

--- Sources/XcbeautifyLib/String+Beautify.swift
+++ Sources/XcbeautifyLib/String+Beautify.swift
@@ -347,8 +347,18 @@ extension String {
             return nil
         case .testCaseMeasured:
             let testCase = groups[1]
-            let time = groups[2]
-            return _colored ? indent + TestStatus.measure.rawValue.foreground.Yellow + " "  + testCase + " measured (\(time.coloredTime()) seconds)" : indent + TestStatus.measure.rawValue + " "  + testCase + " measured (\(time) seconds)"
+            let name = groups[2]
+            let unitName = groups[3]
+            let value = groups[4]
+            let deviation = groups[5].coloredDeviation()
+
+            let formattedValue: String
+            if unitName == "seconds" {
+                formattedValue = value.coloredTime()
+            } else {
+                formattedValue = value
+            }
+            return indent + (_colored ? TestStatus.measure.rawValue.foreground.Yellow : TestStatus.measure.rawValue) + " "  + testCase + " measured (\(formattedValue) \(unitName) ±\(deviation)% -- \(name))"
         case .parallelTestCasePassed:
             let testCase = groups[1]
             let device = groups[2]
@@ -485,10 +495,21 @@ extension String {
     }
 
     private func coloredTime() -> String {
-        guard let time = Double(self) else { return self }
+        guard _colored,
+              let time = Double(self)
+        else { return self }
         if time < 0.025 { return self }
-        if time < 0.100 { return _colored ? f.Yellow : self }
-        return _colored ? f.Red : self
+        if time < 0.100 { return f.Yellow }
+        return f.Red
+    }
+
+    private func coloredDeviation() -> String {
+        guard _colored,
+              let deviation = Double(self)
+        else { return self }
+        if deviation < 1 { return self }
+        if deviation < 10 { return f.Yellow }
+        return f.Red
     }
 
     private func formatPackageStart() -> String? {
