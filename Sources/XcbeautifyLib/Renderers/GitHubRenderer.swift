90fc821ac0650cc072ee4a87d23a5a50aff2c176
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2023-07-02 12:43:40 -0400

    Prevent colored output when using GitHubRenderer

--- Sources/XcbeautifyLib/Renderers/GitHubRenderer.swift
+++ Sources/XcbeautifyLib/Renderers/GitHubRenderer.swift
@@ -7,221 +7,7 @@ struct GitHubRenderer: OutputRendering {
         case error
     }
 
-    let colored = false
-
-    func formatAnalyze(group: AnalyzeCaptureGroup) -> String {
-        let filename = group.fileName
-        let target = group.target
-        return "[\(target)] Analyzing \(filename)"
-    }
-
-    func formatCleanRemove(group: CleanRemoveCaptureGroup) -> String {
-        let directory = group.directory
-        return "Cleaning \(directory)"
-    }
-
-    func formatCodeSign(group: CodesignCaptureGroup) -> String {
-        let command = "Signing"
-        let sourceFile = group.file
-        return command + " " + sourceFile.lastPathComponent
-    }
-
-    func formatCodeSignFramework(group: CodesignFrameworkCaptureGroup) -> String {
-        let frameworkPath = group.frameworkPath
-        return "Signing \(frameworkPath)"
-    }
-
-    func formatProcessPch(group: ProcessPchCaptureGroup) -> String {
-        let filename = group.file
-        let target = group.buildTarget
-        return "[\(target)] Processing \(filename)"
-    }
-
-    func formatProcessPchCommand(group: ProcessPchCommandCaptureGroup) -> String {
-        let filePath = group.filePath
-        return "Preprocessing \(filePath)"
-    }
-
-    func formatCompileCommand(group: CompileCommandCaptureGroup) -> String? {
-        return nil
-    }
-
-    func formatCompile(group: CompileFileCaptureGroup) -> String {
-        let filename = group.filename
-        let target = group.target
-        return "[\(target)] Compiling \(filename)"
-    }
-
-    func formatCopy(group: CopyCaptureGroup) -> String {
-        let filename = group.file
-        let target = group.target
-        return "[\(target)] Copying \(filename)"
-    }
-
-    func formatGenerateDsym(group: GenerateDSYMCaptureGroup) -> String {
-        let dsym = group.dsym
-        let target = group.target
-        return "[\(target)] Generating \(dsym)"
-    }
-
-    func formatGenerateCoverageData(group: GenerateCoverageDataCaptureGroup) -> String {
-        return "Generating code coverage data..."
-    }
-
-    func formatCoverageReport(group: GeneratedCoverageReportCaptureGroup) -> String {
-        let filePath = group.coverageReportFilePath
-        return "Generated code coverage report: \(filePath)"
-    }
-
-    func formatLibtool(group: LibtoolCaptureGroup) -> String {
-        let filename = group.fileName
-        let target = group.target
-        return "[\(target)] Building library \(filename)"
-    }
-
-    func formatTouch(group: TouchCaptureGroup) -> String {
-        let filename = group.filename
-        let target = group.target
-        return "[\(target)] Touching \(filename)"
-    }
-
-    func formatPhaseSuccess(group: PhaseSuccessCaptureGroup) -> String {
-        let phase = group.phase.capitalized
-        return "\(phase) Succeeded"
-    }
-
-    func formatLinking(group: LinkingCaptureGroup) -> String {
-        let target = group.target
-#if os(Linux)
-        return "[\(target)] Linking"
-#else
-        let filename = group.binaryFilename
-        return "[\(target)] Linking \(filename)"
-#endif
-    }
-
-    func formatPhaseScriptExecution(group: PhaseScriptExecutionCaptureGroup) -> String {
-        let phaseName = group.phaseName
-        let target = group.target
-        // Strip backslashed added by xcodebuild before spaces in the build phase name
-        let strippedPhaseName = phaseName.replacingOccurrences(of: "\\ ", with: " ")
-        return "[\(target)] Running script \(strippedPhaseName)"
-    }
-
-    func formatTestSuiteStart(group: TestSuiteStartCaptureGroup) -> String {
-        let testSuite = group.testSuiteName
-        return testSuite
-    }
-
-    func formatTestSuiteStarted(group: TestSuiteStartedCaptureGroup) -> String {
-        let testSuite = group.suite
-        let heading = "Test Suite \(testSuite) started"
-        return heading
-    }
-
-    func formatParallelTestSuiteStarted(group: ParallelTestSuiteStartedCaptureGroup) -> String {
-        let testSuite = group.suite
-        let deviceDescription = " on '\(group.device)'"
-        let heading = "Test Suite \(testSuite) started\(deviceDescription)"
-        return heading
-    }
-
-    func formatParallelTestingStarted(line: String, group: ParallelTestingStartedCaptureGroup) -> String {
-        return line
-    }
-
-    func formatParallelTestingPassed(line: String, group: ParallelTestingPassedCaptureGroup) -> String {
-        return line
-    }
-
-    func formatTestCasePassed(group: TestCasePassedCaptureGroup) -> String {
-        // TODO: Extract to shared property
-        let indent = "    "
-        let testCase = group.testCase
-        let time = group.time
-        return indent + TestStatus.pass + " " + testCase + " (\(time) seconds)"
-    }
-
-    func formatTestCasePending(group: TestCasePendingCaptureGroup) -> String {
-        let indent = "    "
-        let testCase = group.testCase
-        return indent + TestStatus.pending + " "  + testCase + " [PENDING]"
-    }
-
-    func formatTestCaseMeasured(group: TestCaseMeasuredCaptureGroup) -> String {
-        let indent = "    "
-        let testCase = group.testCase
-        let name = group.name
-        let unitName = group.unitName
-        let value = group.value
-        let deviation = group.deviation
-
-        let formattedValue: String
-        if unitName == "seconds" {
-            formattedValue = value
-        } else {
-            formattedValue = value
-        }
-        return indent + TestStatus.measure + " "  + testCase + " measured (\(formattedValue) \(unitName) ±\(deviation)% -- \(name))"
-    }
-
-    func formatParallelTestCasePassed(group: ParallelTestCasePassedCaptureGroup) -> String {
-        let indent = "    "
-        let testCase = group.testCase
-        let device = group.device
-        let time = group.time
-        return indent + TestStatus.pass + " " + testCase + " on '\(device)' (\(time) seconds)"
-    }
-
-    func formatParallelTestCaseAppKitPassed(group: ParallelTestCaseAppKitPassedCaptureGroup) -> String {
-        let indent = "    "
-        let testCase = group.testCase
-        let time = group.time
-        return indent + TestStatus.pass + " " + testCase + " (\(time)) seconds)"
-    }
-
-    func formatProcessInfoPlist(group: ProcessInfoPlistCaptureGroup) -> String {
-        let plist = group.filename
-
-        if let target = group.target {
-            // Xcode 10+ output
-            return "[\(target)] \("Processing") \(plist)"
-        } else {
-            // Xcode 9 output
-            return "Processing" + " " + plist
-        }
-    }
-
-    func formatPackageFetching(group: PackageFetchingCaptureGroup) -> String {
-        let source = group.source
-        return "Fetching " + source
-    }
-
-    func formatPackageUpdating(group: PackageUpdatingCaptureGroup) -> String {
-        let source = group.source
-        return "Updating " + source
-    }
-
-    func formatPackageCheckingOut(group: PackageCheckingOutCaptureGroup) -> String {
-        let version = group.version
-        let package = group.package
-        return "Checking out \(package) @ \(version)"
-    }
-
-    func formatPackageStart() -> String {
-        return "Resolving Package Graph"
-    }
-
-    func formatPackageEnd() -> String {
-        return "Resolved source packages"
-    }
-
-    func formatPackageItem(group: PackageGraphResolvedItemCaptureGroup) -> String  {
-        let name = group.packageName
-        let url = group.packageURL
-        let version = group.packageVersion
-        return "\(name) - \(url) @ \(version)"
-    }
+    let colored: Bool = false
 
     private func outputGitHubActionsLog(
         annotationType: AnnotationType,
@@ -229,7 +15,7 @@ struct GitHubRenderer: OutputRendering {
         message: String
     ) -> String {
         let formattedFileComponents = fileComponents?.formatted ?? ""
-        return "::\(annotationType.rawValue) \(formattedFileComponents)::\(message)"
+        return "::\(annotationType) \(formattedFileComponents)::\(message)"
     }
 
     func formatCompileError(group: CompileErrorCaptureGroup, additionalLines: @escaping () -> (String?)) -> String {
@@ -241,14 +27,7 @@ struct GitHubRenderer: OutputRendering {
         let line: String = additionalLines() ?? ""
         let cursor: String = additionalLines() ?? ""
 
-        let message = colored ?
-            """
-            \(Symbol.error): \(reason.f.Red)
-            \(line)
-            \(cursor.f.Cyan)
-            """
-            :
-            """
+        let message = """
             \(Symbol.asciiError): \(reason)
             \(line)
             \(cursor)
@@ -270,14 +49,7 @@ struct GitHubRenderer: OutputRendering {
         let line: String = additionalLines() ?? ""
         let cursor: String = additionalLines() ?? ""
 
-        let message = colored ?
-            """
-            \(Symbol.warning): \(reason.f.Yellow)
-            \(line)
-            \(cursor.f.Green)
-            """
-            :
-            """
+        let message = """
             \(Symbol.asciiWarning): \(reason)
             \(line)
             \(cursor)
@@ -293,14 +65,14 @@ struct GitHubRenderer: OutputRendering {
     func formatCompleteError(line: String) -> String {
         return outputGitHubActionsLog(
             annotationType: .error,
-            message: colored ? Symbol.error + " " + line.f.Red : Symbol.asciiError + " " + line
+            message: Symbol.asciiError + " " + line
         )
     }
 
     func formatCompleteWarning(line: String) -> String {
         return outputGitHubActionsLog(
             annotationType: .warning,
-            message: colored ? Symbol.warning + " " + line.f.Yellow : Symbol.asciiWarning + " " + line
+            message: Symbol.asciiWarning + " " + line
         )
     }
 
@@ -308,7 +80,7 @@ struct GitHubRenderer: OutputRendering {
         let message = group.warningMessage
         return outputGitHubActionsLog(
             annotationType: .warning,
-            message: colored ? Symbol.warning + " " + message.f.Yellow : Symbol.asciiWarning + " " + message
+            message: Symbol.asciiWarning + " " + message
         )
     }
 
@@ -316,7 +88,7 @@ struct GitHubRenderer: OutputRendering {
         let errorMessage = group.wholeError
         return outputGitHubActionsLog(
             annotationType: .error,
-            message: colored ? Symbol.error + " " + errorMessage.f.Red : Symbol.asciiError + " " + errorMessage
+            message: Symbol.asciiError + " " + errorMessage
         )
     }
 
@@ -326,7 +98,7 @@ struct GitHubRenderer: OutputRendering {
         let fileComponents = file.asFileComponents()
         let testCase = group.testCase
         let failingReason = group.reason
-        let message = colored ? indent + TestStatus.fail.foreground.Red + " "  + testCase + ", " + failingReason : indent + TestStatus.fail + " "  + testCase + ", " + failingReason
+        let message = indent + TestStatus.fail + " "  + testCase + ", " + failingReason
         return outputGitHubActionsLog(
             annotationType: .error,
             fileComponents: fileComponents,
@@ -338,7 +110,7 @@ struct GitHubRenderer: OutputRendering {
         let reason = group.reason
         let filePath = group.filePath
         let fileComponents = filePath.asFileComponents()
-        let message = colored ? "\(Symbol.error): \(reason.f.Red)" : "\(Symbol.asciiError): \(reason)"
+        let message = "\(Symbol.asciiError): \(reason)"
         return outputGitHubActionsLog(
             annotationType: .error,
             fileComponents: fileComponents,
@@ -349,7 +121,7 @@ struct GitHubRenderer: OutputRendering {
     func formatLdWarning(group: LDWarningCaptureGroup) -> String {
         let prefix = group.ldPrefix
         let warningMessage = group.warningMessage
-        let message = colored ? "\(Symbol.warning) \(prefix.f.Yellow)\(warningMessage.f.Yellow)" : "\(Symbol.asciiWarning) \(prefix)\(warningMessage)"
+        let message = "\(Symbol.asciiWarning) \(prefix)\(warningMessage)"
         return outputGitHubActionsLog(
             annotationType: .warning,
             message: message
@@ -358,13 +130,13 @@ struct GitHubRenderer: OutputRendering {
 
     func formatLinkerDuplicateSymbolsError(group: LinkerDuplicateSymbolsCaptureGroup) -> String {
         let reason = group.reason
-        let message = colored ? "\(Symbol.error) \(reason.f.Red)" : "\(Symbol.asciiError) \(reason)"
+        let message = "\(Symbol.asciiError) \(reason)"
         return outputGitHubActionsLog(annotationType: .error, message: message)
     }
 
     func formatLinkerUndefinedSymbolsError(group: LinkerUndefinedSymbolsCaptureGroup) -> String {
         let reason = group.reason
-        let message = colored ? "\(Symbol.error) \(reason.f.Red)" : "\(Symbol.asciiError) \(reason)"
+        let message = "\(Symbol.asciiError) \(reason)"
         return outputGitHubActionsLog(annotationType: .error, message: message)
     }
 
@@ -372,7 +144,7 @@ struct GitHubRenderer: OutputRendering {
         let testCase = group.testCase
         let device = group.device
         let time = group.time
-        let message = colored ? "    \(TestStatus.fail.f.Red) \(testCase) on '\(device)' (\(time.coloredTime(colored: colored)) seconds)" : "    \(TestStatus.fail) \(testCase) on '\(device)' (\(time) seconds)"
+        let message = "    \(TestStatus.fail) \(testCase) on '\(device)' (\(time) seconds)"
         return outputGitHubActionsLog(
             annotationType: .error,
             message: message
@@ -380,16 +152,15 @@ struct GitHubRenderer: OutputRendering {
     }
 
     func formatParallelTestingFailed(line: String, group: ParallelTestingFailedCaptureGroup) -> String {
-        let message = colored ? line.s.Bold.f.Red : line
         return outputGitHubActionsLog(
             annotationType: .error,
-            message: message
+            message: line
         )
     }
 
     func formatRestartingTest(line: String, group: RestartingTestCaptureGroup) -> String {
         let indent = "    "
-        let message = colored ? indent + TestStatus.fail.foreground.Red + " "  + line : indent + TestStatus.fail + " "  + line
+        let message = indent + TestStatus.fail + " "  + line
         return outputGitHubActionsLog(
             annotationType: .error,
             message: message
@@ -401,7 +172,7 @@ struct GitHubRenderer: OutputRendering {
         let file = group.file
         let fileComponents = file.asFileComponents()
         let failingReason = group.reason
-        let message = colored ? indent + TestStatus.fail.foreground.Red + ": " + failingReason : indent + TestStatus.fail + ": " + failingReason
+        let message = indent + TestStatus.fail + ": " + failingReason
         return outputGitHubActionsLog(
             annotationType: .error,
             fileComponents: fileComponents,
@@ -411,7 +182,7 @@ struct GitHubRenderer: OutputRendering {
 
     func formatWarning(group: GenericWarningCaptureGroup) -> String {
         let warningMessage = group.wholeWarning
-        let message = colored ? Symbol.warning + " " + warningMessage.f.Yellow : Symbol.asciiWarning + " " + warningMessage
+        let message = Symbol.asciiWarning + " " + warningMessage
         return outputGitHubActionsLog(
             annotationType: .warning,
             message: message
@@ -420,7 +191,7 @@ struct GitHubRenderer: OutputRendering {
 
     func formatWillNotBeCodesignWarning(group: WillNotBeCodeSignedCaptureGroup) -> String {
         let warningMessage = group.wholeWarning
-        let message = colored ? Symbol.warning + " " + warningMessage.f.Yellow : Symbol.asciiWarning + " " + warningMessage
+        let message = Symbol.asciiWarning + " " + warningMessage
         return outputGitHubActionsLog(
             annotationType: .warning,
             message: message
