e61f1c4bb2402d03c0c9bce373d554f4803c18c4
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2023-07-01 20:35:39 -0400

    Refactor captureGroup

--- Sources/XcbeautifyLib/String+CapturedGroups.swift
+++ Sources/XcbeautifyLib/String+CapturedGroups.swift
@@ -2,29 +2,28 @@ import Foundation
 
 extension String {
     private func captureGroup(with pattern: Pattern) -> [String] {
-        var results = [String]()
-
-        var regex: NSRegularExpression
         do {
-            regex = try NSRegularExpression(pattern: pattern.rawValue, options: [.caseInsensitive])
-        } catch {
+            let regex = try NSRegularExpression(pattern: pattern.rawValue, options: [.caseInsensitive])
+
+            let matches = regex.matches(in: self, range: NSRange(location:0, length: self.utf16.count))
+            guard let match = matches.first else { return [] }
+
+            let lastRangeIndex = match.numberOfRanges - 1
+            guard lastRangeIndex >= 1 else { return [] }
+
+            var results = [String]()
+
+            for i in 1...lastRangeIndex {
+                let capturedGroupIndex = match.range(at: i)
+                guard let matchedString = substring(with: capturedGroupIndex) else { continue }
+                results.append(matchedString)
+            }
+
             return results
+        } catch {
+            assertionFailure(error.localizedDescription)
+            return []
         }
-
-        let matches = regex.matches(in: self, range: NSRange(location:0, length: self.utf16.count))
-
-        guard let match = matches.first else { return results }
-
-        let lastRangeIndex = match.numberOfRanges - 1
-        guard lastRangeIndex >= 1 else { return results }
-
-        for i in 1...lastRangeIndex {
-            let capturedGroupIndex = match.range(at: i)
-            guard let matchedString = substring(with: capturedGroupIndex) else { continue }
-            results.append(matchedString)
-        }
-
-        return results
     }
 }
 
