5939f78d1afafecca259d268a4e649fd6f35df58
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2024-01-13 21:37:26 -0500

    Create Formatter.swift

---
+++ Sources/XcbeautifyLib/Formatter.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+package class Formatter {
+
+    private let colored: Bool
+    private let renderer: OutputRendering
+    private let additionalLines: () -> String?
+    package var preserveUnbeautifiedLines = false
+
+    package init(
+        colored: Bool = true,
+        renderer: Renderer,
+        preserveUnbeautifiedLines: Bool = false,
+        additionalLines: @escaping () -> (String?)
+    ) {
+        self.colored = colored
+
+        switch renderer {
+        case .terminal:
+            self.renderer = TerminalRenderer(colored: colored, additionalLines: additionalLines)
+        case .gitHubActions:
+            self.renderer = GitHubActionsRenderer(additionalLines: additionalLines)
+        case .teamcity:
+            self.renderer = TeamCityRenderer(colored: colored, additionalLines: additionalLines)
+        }
+
+        self.preserveUnbeautifiedLines = preserveUnbeautifiedLines
+        self.additionalLines = additionalLines
+    }
+
+}
