f137a6983f13614634419c06d454fb4d794a41c2
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2024-01-14 17:18:35 -0500

    Delete OutputType+CustomStringConvertible

--- Sources/XcbeautifyLib/Constants.swift
+++ Sources/XcbeautifyLib/Constants.swift
@@ -31,29 +31,6 @@ public enum OutputType {
     case result
 }
 
-extension OutputType: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .undefined:
-            return "undefined"
-        case .task:
-            return "task"
-        case .test:
-            return "test"
-        case .warning:
-            return "warning"
-        case .error:
-            return "error"
-        case .result:
-            return "result"
-        case .testCase:
-            return "testCase"
-        case .nonContextualError:
-            return "nonContextualError"
-        }
-    }
-}
-
 /// Maps to an `OutputRendering` type that formats raw `xcodebuild` output.
 public enum Renderer: String {
     /// The default `OutputRendering` type for local and general use. Maps to `TerminalRenderer`.
