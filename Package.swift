bb67595bcd8bf9ea684194b11a48aa7a7ebd1be9
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2024-06-21 22:28:47 -0400

    Enable InferSendableFromCaptures

--- Package.swift
+++ Package.swift
@@ -63,6 +63,7 @@ for target in package.targets {
             .enableUpcomingFeature("GlobalConcurrency"),
             .enableUpcomingFeature("ImplicitOpenExistentials"),
             .enableUpcomingFeature("ImportObjcForwardDeclarations"),
+            .enableUpcomingFeature("InferSendableFromCaptures"),
         ]
     )
     target.swiftSettings = settings
