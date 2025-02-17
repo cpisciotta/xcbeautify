7c94b6092b877737817f24aa27cf631b17f47c0b
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2025-02-17 13:24:51 -0500

    Add testFullSPISwiftTestingOutput

--- Tests/XcbeautifyLibTests/ParsingTests.swift
+++ Tests/XcbeautifyLibTests/ParsingTests.swift
@@ -117,6 +117,33 @@ final class ParsingTests: XCTestCase {
         #endif
     }
 
+    func testFullSPISwiftTestingOutput() throws {
+        let url = Bundle.module.url(forResource: "spi_swift_testing_full_log", withExtension: "txt")!
+        let logContent = try String(contentsOf: url)
+        var buildLog = logContent.components(separatedBy: .newlines)
+        let parser = Parser()
+        var uncapturedOutput = 0
+
+        while !buildLog.isEmpty {
+            let line = buildLog.removeFirst()
+            if !line.isEmpty, parser.parse(line: line) == nil {
+                uncapturedOutput += 1
+            }
+        }
+
+        // The following is a magic number.
+        // It represents the number of lines that aren't captured by the Parser.
+        // Slowly, this value should decrease until it reaches 0.
+        // It uses `XCTAssertEqual` instead of `XCTAssertLessThanOrEqual` as a reminder.
+        // Update this magic number whenever `uncapturedOutput` is less than the current magic number.
+        // There's a regression whenever `uncapturedOutput` is greater than the current magic number.
+        #if os(macOS)
+        XCTAssertEqual(uncapturedOutput, 2199)
+        #else
+        XCTAssertEqual(uncapturedOutput, 2198)
+        #endif
+    }
+
     func testDemoSwiftTestingOutput() throws {
         let url = Bundle.module.url(forResource: "demo_swift_testing_log", withExtension: "txt")!
         let logContent = try String(contentsOf: url)
