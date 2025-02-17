45520600a5c99fa98c044ab7afd4771400a186ad
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2025-02-17 12:39:44 -0500

    Partially Revert Swift Testing Capture Group Changes

--- Sources/XcbeautifyLib/CaptureGroups.swift
+++ Sources/XcbeautifyLib/CaptureGroups.swift
@@ -1991,7 +1991,7 @@ struct SwiftTestingRunStartedCaptureGroup: CaptureGroup {
 
     /// Regular expression to capture the start of a test run.
     /// $1 = message
-    static let regex = XCRegex(pattern: #"^􀟈  (Test run started\.)$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+(Test run started\.)$"#)
 
     let message: String
 
@@ -2008,7 +2008,7 @@ struct SwiftTestingRunCompletionCaptureGroup: CaptureGroup {
     /// Regular expression to capture the number of tests and total time.
     /// $1 = number of tests
     /// $2 = total time in seconds
-    static let regex = XCRegex(pattern: #"^􁁛  Test run with (\d+) test(?:s)? passed after ([\d.]+) seconds\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test run with (\d+) test(?:s)? passed after ([\d.]+) seconds\.$"#)
 
     let numberOfTests: Int
     let totalTime: String
@@ -2029,7 +2029,7 @@ struct SwiftTestingRunFailedCaptureGroup: CaptureGroup {
     /// $1 = number of tests
     /// $2 = total time in seconds
     /// $3 = number of issues
-    static let regex = XCRegex(pattern: #"^􀢄  Test run with (\d+) test(?:s)? failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test run with (\d+) test(?:s)? failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
 
     let numberOfTests: Int
     let totalTime: String
@@ -2051,7 +2051,7 @@ struct SwiftTestingSuiteStartedCaptureGroup: CaptureGroup {
 
     /// Regular expression to capture the start of a test suite.
     /// $1 = suite name
-    static let regex = XCRegex(pattern: #"^􀟈  Suite (.*) started\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Suite (.*) started\.$"#)
 
     let suiteName: String
 
@@ -2067,7 +2067,7 @@ struct SwiftTestingTestStartedCaptureGroup: CaptureGroup {
 
     /// Regular expression to capture the start of a test case.
     /// $1 = test name
-    static let regex = XCRegex(pattern: #"^􀟈  Test (?!run )(.*) started\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (?!run )(.*) started\.$"#)
 
     let testName: String
     let wholeMessage: String
@@ -2086,7 +2086,7 @@ struct SwiftTestingSuitePassedCaptureGroup: CaptureGroup {
     /// Regular expression to capture the successful completion of a test suite.
     /// $1 = suite name
     /// $2 = total time taken in seconds
-    static let regex = XCRegex(pattern: #"^􁁛  Suite (.*) passed after ([\d.]+) seconds\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Suite (.*) passed after ([\d.]+) seconds\.$"#)
 
     let suiteName: String
     let timeTaken: String
@@ -2107,7 +2107,7 @@ struct SwiftTestingSuiteFailedCaptureGroup: CaptureGroup {
     /// $1 = suite name
     /// $2 = total time taken in seconds
     /// $3 = number of issues
-    static let regex = XCRegex(pattern: #"^􀢄  Suite (.*) failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Suite (.*) failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
 
     let suiteName: String
     let timeTaken: String
@@ -2131,7 +2131,7 @@ struct SwiftTestingTestFailedCaptureGroup: CaptureGroup {
     /// $1 = test name
     /// $2 = time taken in seconds
     /// $3 = number of issues
-    static let regex = XCRegex(pattern: #"^􀢄  Test (?!run\s)(.*) failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (?!run\s)(.*) failed after ([\d.]+) seconds with (\d+) issue[s]?\.$"#)
 
     let testName: String
     let timeTaken: String
@@ -2154,7 +2154,7 @@ struct SwiftTestingTestPassedCaptureGroup: CaptureGroup {
     /// Regular expression to capture the successful completion of a test case.
     /// $1 = test name
     /// $2 = time taken in seconds
-    static let regex = XCRegex(pattern: #"^􁁛  Test (?!run\s)(.*) passed after ([\d.]+) seconds\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (?!run\s)(.*) passed after ([\d.]+) seconds\.$"#)
 
     let testName: String
     let timeTaken: String
@@ -2173,7 +2173,7 @@ struct SwiftTestingTestSkippedCaptureGroup: CaptureGroup {
 
     /// Regular expression to capture a skipped test case.
     /// $1 = test name
-    static let regex = XCRegex(pattern: #"^􀙟  Test (.*) skipped\.$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (.*) skipped\.$"#)
 
     let testName: String
 
@@ -2190,7 +2190,7 @@ struct SwiftTestingTestSkippedReasonCaptureGroup: CaptureGroup {
     /// Regular expression to capture a skipped test case with a reason.
     /// $1 = test name
     /// $2 = optional reason
-    static let regex = XCRegex(pattern: #"^􀙟  Test (.*) skipped(?:\s*:\s*"(.*)")?$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (.*) skipped(?:\s*:\s*"(.*)")?$"#)
 
     let testName: String
     let reason: String?
@@ -2209,7 +2209,7 @@ struct SwiftTestingIssueCaptureGroup: CaptureGroup {
     /// Regular expression to capture the symbol, test description, and issue details.
     /// $1 = test description
     /// $2 = issue details
-    static let regex = XCRegex(pattern: #"^􀢄  Test (.*?) recorded an issue at (.*)$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (.*?) recorded an issue at (.*)$"#)
 
     let testDescription: String
     let issueDetails: String?
@@ -2229,7 +2229,7 @@ struct SwiftTestingIssueArgumentCaptureGroup: CaptureGroup {
     /// Regular expression to capture the symbol, test description, and optional number of arguments.
     /// $1 = test description
     /// $2 = number of arguments (optional)
-    static let regex = XCRegex(pattern: #"^􀢄  Test (.*?) recorded an issue with (\d+) arguments?"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Test (.*?) recorded an issue with (\d+) arguments?"#)
 
     let testDescription: String
     let numberOfArguments: Int?
@@ -2248,7 +2248,7 @@ struct SwiftTestingPassingArgumentCaptureGroup: CaptureGroup {
 
     /// Regular expression to capture the symbol and number of arguments.
     /// $1 = number of arguments
-    static let regex = XCRegex(pattern: #"^􀟈  Passing (\d+) argument[s]?.*$"#)
+    static let regex = XCRegex(pattern: #"^[^\s]\s+Passing (\d+) argument[s]?.*$"#)
 
     let numberOfArguments: Int
 
