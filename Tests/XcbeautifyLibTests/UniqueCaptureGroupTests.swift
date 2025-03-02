5931fc5a2cefc2bce1d860929fbbe514ce5ce210
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2025-03-02 14:20:53 -0500

    Fix LDWarningCaptureGroup Collisions (#396)

--- Tests/XcbeautifyLibTests/UniqueCaptureGroupTests.swift
+++ Tests/XcbeautifyLibTests/UniqueCaptureGroupTests.swift
@@ -81,6 +81,26 @@ final class UniqueCaptureGroupTests: XCTestCase {
         )
     }
 
+    func testUniqueLDWarning() {
+        let line = "ld: warning: embedded dylibs/frameworks only run on iOS 8 or later"
+
+        let capturedTypes = captureGroupTypes.filter { type in
+            guard let groups = type.regex.captureGroups(for: line) else { return false }
+            XCTAssertNotNil(type.init(groups: groups))
+            return true
+        }
+
+        XCTAssertEqual(
+            capturedTypes.count,
+            1,
+            """
+            Failed to uniquely parse xcodebuild output.
+            Line: \(line)
+            Captured Types: \(ListFormatter.localizedString(byJoining: capturedTypes.map(String.init(describing:))))
+            """
+        )
+    }
+
     func testUniqueTestCaptureGroups() throws {
         let url = try XCTUnwrap(Bundle.module.url(forResource: "TestLog", withExtension: "txt"))
         var buildLog: [String] = try String(contentsOf: url)
