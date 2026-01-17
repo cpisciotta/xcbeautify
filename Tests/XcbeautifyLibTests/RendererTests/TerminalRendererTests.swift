fc4679776eeb8aa2b1556618e8c7ce9a3411eb57
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2026-01-17 14:23:56 -0500

    Update Copyright Notice (#464)

--- Tests/XcbeautifyLibTests/RendererTests/TerminalRendererTests.swift
+++ Tests/XcbeautifyLibTests/RendererTests/TerminalRendererTests.swift
@@ -1,7 +1,7 @@
 //
 // TerminalRendererTests.swift
 //
-// Copyright (c) 2025 Charles Pisciotta and other contributors
+// Copyright (c) 2026 Charles Pisciotta and other contributors
 // Licensed under MIT License
 //
 // See https://github.com/cpisciotta/xcbeautify/blob/main/LICENSE for license information
