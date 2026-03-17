6240d1a656d45df98457816ea6be0907db2df8cd
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2026-03-16 21:04:04 -0400

    Address SwiftFormat issues

--- Sources/XcbeautifyLib/JUnitReporter.swift
+++ Sources/XcbeautifyLib/JUnitReporter.swift
@@ -23,9 +23,9 @@ package protocol JUnitParallelReportable: JUnitReportable { }
 
 package final class JUnitReporter {
     private var components: [JUnitComponent] = []
-    // Parallel output does not guarantee order - so it is _very_ hard
-    // to match to the parent suite. We can still capture test success/failure
-    // and output a generic result file.
+    /// Parallel output does not guarantee order - so it is _very_ hard
+    /// to match to the parent suite. We can still capture test success/failure
+    /// and output a generic result file.
     private var parallelComponents: [JUnitComponent] = []
 
     package init() { }
